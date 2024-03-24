1c16f556ae9987af52e63ccebea70a3a382a28e8
Lambodar Bag <152137585+lambodarbag@users.noreply.github.com>
2024-03-24 13:58:39 +0530

    Updated

--- Calculator/script.js
+++ Calculator/script.js
@@ -10,16 +10,94 @@ const clear = document.querySelector(".clear");
 const dot = document.querySelector(".dot");
 const equal = document.querySelector(".equal");
 
-let result = "";
 
-const display = (number) => {
+let previousOperand = "";
+let result= "";
+let operator = "";
+
+
+function appendNumber(number) {
+    if (number === "." && result.includes("."))return;
     result += number;
-    input.value = result;
+    console.log(result);
+    console.log(previousOperand);
+    updateDisplay();
 }
 
 
-buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        display(button.textContent);
+const updateDisplay = () => {
+    if (operator) {
+        input.value = `${previousOperand}${operator}${result}`
+    }else {
+        input.value = result;
+    }
+}
+
+function selectOperator (operatorValue) {
+    if (result == "") return;
+
+    if (operator !== "" && previousOperand !== "") {
+        calculateResult();
+    }
+
+    operator = operatorValue;
+    previousOperand = result;
+    result = "";
+    updateDisplay();
+}
+
+
+function calculateResult() {
+    let evalutedResult;
+    const prev = Number(previousOperand);
+    const current = Number(result);
+
+    if (isNaN(prev) || isNaN(current))return;
+
+    switch (operator) {
+        case '+' : 
+            evalutedResult = prev + current;
+            break;
+        case '-' :
+            evalutedResult = prev - current;
+            break;
+        case '*' :
+            evalutedResult = prev * current;
+            break;
+        case '/' : 
+            evalutedResult = prev / current;
+            break;
+        case '%' :
+            evalutedResult = prev % current;
+            break;
+        default :
+            return;
+    }
+
+    result = evalutedResult.toString();
+    operator = "";
+    previousOperand = "";
+    updateDisplay();
+}
+
+
+buttons.forEach(button =>  {
+    button.addEventListener("click", function() {
+        appendNumber(button.textContent);
     });
+});
+
+
+
+
+dot.addEventListener("click", () => appendNumber(".") );
+add.addEventListener("click", () => selectOperator("+") );
+minus.addEventListener("click", () => selectOperator("-") );
+multiply.addEventListener("click", () => selectOperator("*") );
+divide.addEventListener("click", () => selectOperator("/") );
+percent.addEventListener("click", () => selectOperator("%") );
+equal.addEventListener("click", () => {
+    if (result === "") return;
+    calculateResult();
+    updateDisplay();
 });
